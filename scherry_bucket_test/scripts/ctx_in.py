961fa57dec07d36a69acc116ff8ab1455101405c
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-01-17 17:45:18 -0800

    bucket(test): add scripts

---
+++ scherry_bucket_test/scripts/ctx_in.py
@@ -0,0 +1,9 @@
+
+seq = globals().get("ScherrySeq")
+
+print("current sequence is " + seq)
+
+dataOut = {
+    "a" : 1,
+    "b" : 2
+}
---
+++ scherry_bucket_test/scripts/ctx_out.py
@@ -0,0 +1,8 @@
+
+
+seq = globals().get("ScherrySeq")
+
+print("current sequence is " + seq)
+
+assert globals().get("a") == 1
+assert globals().get("b") == 2
