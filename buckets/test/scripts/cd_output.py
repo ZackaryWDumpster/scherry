035b341ff5fa3f6346320bb93c7d1de53c807989
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-01-19 07:46:28 -0800

    bucket(test): update

--- buckets/test/scripts/cd_output.py
+++
@@ -1,7 +0,0 @@
-
-import os
-
-
-if not os.path.exists("output"):
-    os.makedirs("output")
-os.chdir("output")
---
+++ buckets/test/scripts/expose_mgr.py
@@ -0,0 +1,4 @@
+from scherry.core.mgr import ScherryMgr
+
+mgr = ScherryMgr()
+print(mgr.bucket_list_installed())
