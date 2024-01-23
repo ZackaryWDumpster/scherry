95868a1a66fbf1baddd1ab71980df7d740b2f171
ZackaryW <36378555+ZackaryW@users.noreply.github.com>
2024-01-22 21:35:09 -0800

    feat: add global default cfg

--- scherry/core/__init__.py
+++ scherry/core/__init__.py
@@ -1,6 +1,9 @@
 
+import functools
 import os
-from scherry.utils.auto_save_dict import AutoSaveDict
+import typing
+from typing import TypedDict
+from scherry.utils.auto_save_dict import AutoSaveDict, AutoSaveDictChild
 
 # [appdata]
 core_dir = os.path.dirname(os.path.realpath(__file__))
@@ -28,3 +31,17 @@ os.makedirs(bucket_cache_dir, exist_ok=True)
 
 # [appdata/cfg.json]
 cfg = AutoSaveDict(os.path.join(appdata_dir, "cfg.json"))
+
+
+# [cfg/setting]
+class _setting_td(TypedDict):
+    persist_data_after_run : bool
+    drop_underscored_vars : bool
+
+_default_setting : _setting_td = {
+    "persist_data_after_run": True,
+    "drop_underscored_vars": True
+}
+
+default_setting : typing.Union[AutoSaveDictChild, _setting_td] = cfg.ensure_child("default")
+default_setting.setReference(_default_setting)
--- scherry/utils/auto_save_dict.py
+++ scherry/utils/auto_save_dict.py
@@ -1,34 +1,14 @@
 import os
 import shutil
+import typing
 import orjson
-
 from scherry.utils.dictionary import setDeep as _setDeep
 from scherry.utils.dictionary import getDeep as _getDeep
 from scherry.utils.dictionary import ERROR
 
-class AutoSaveDict(dict):
-    def __init__(self, filename, *args, bkup : bool = False, **kwargs):
-        self.filename = filename
-        self.__bkup = bkup
-        super().__init__(*args, **kwargs)
-        if not os.path.exists(self.filename):
-            with open(self.filename, 'w') as f:
-                f.write('{}')
-        try:
-            self._load()
-        except: # noqa
-            raise RuntimeError("Failed to load AutoSaveDict")
-            
-    def _save(self):
-        if self.__bkup and os.path.exists(self.filename):
-            shutil.copyfile(self.filename, os.path.join(os.path.dirname(self.filename), f"{os.path.basename(self.filename)}.bkup"))
-        
-        with open(self.filename, 'wb') as f:
-            f.write(orjson.dumps(self, option=orjson.OPT_SERIALIZE_NUMPY | orjson.OPT_INDENT_2))
-            
-    def _load(self):
-        with open(self.filename, 'rb') as f:
-            self.update(orjson.loads(f.read()))
+class _Shared(dict):
+    def ensure_child(self, *args):
+        return self.ensure_type(*args, type_=AutoSaveDictChild)
         
     def __setitem__(self, key, value):
         super().__setitem__(key, value)
@@ -73,4 +53,71 @@ class AutoSaveDict(dict):
         if res is ERROR:
             self.setDeep(*args)
             self._save()    
+            
+    def ensure_type(self, *args, type_ : typing.Type[dict]):
+        res = self.getDeep(*args[:-1])
+        if res is ERROR:
+            ret = type_()
+            self.setDeep(*args[:-1], ret)   
+            return ret
+        elif not isinstance(res, dict):
+            raise RuntimeError(f"expected dict, got {type(res)}")
+        elif type_ == AutoSaveDictChild:
+            res[args[-1]] = AutoSaveDictChild(self, *args[:-1])
+            return res[args[-1]]
+        else:
+            res[args[-1]] = type_(res[args[-1]])
+            return res[args[-1]]
+
+class AutoSaveDict(_Shared):
+    def __init__(self, filename, *args, bkup : bool = False, **kwargs):
+        self.filename = filename
+        self.__bkup = bkup
+        super().__init__(*args, **kwargs)
+        if not os.path.exists(self.filename):
+            with open(self.filename, 'w') as f:
+                f.write('{}')
+        try:
+            self._load()
+        except: # noqa
+            raise RuntimeError("Failed to load AutoSaveDict")
+    
+    def _save(self):
+        if self.__bkup and os.path.exists(self.filename):
+            shutil.copyfile(self.filename, os.path.join(os.path.dirname(self.filename), f"{os.path.basename(self.filename)}.bkup"))
         
+        with open(self.filename, 'wb') as f:
+            f.write(orjson.dumps(self, option=orjson.OPT_SERIALIZE_NUMPY | orjson.OPT_INDENT_2))
+            
+    def _load(self):
+        with open(self.filename, 'rb') as f:
+            self.update(orjson.loads(f.read()))
+            
+        
+class AutoSaveDictChild(_Shared):
+    def __init__(self, parent : AutoSaveDict, *args, **kwargs):
+        self.parent = parent
+        self.__referenceDict = None
+        super().__init__(*args, **kwargs)
+        
+        
+    def _save(self):
+        self.parent._save()
+    
+    def __getitem__(self, __key):
+        if self.__referenceDict is not None and __key not in self.__referenceDict:
+            raise KeyError(__key)
+        
+        if __key not in self:
+            self[__key] = self.__referenceDict[__key]
+            
+        return super().__getitem__(__key)
+        
+    def __setitem__(self, key, value):
+        if self.__referenceDict is not None and key not in self.__referenceDict:
+            raise KeyError(key)
+        
+        return super().__setitem__(key, value)    
+    
+    def setReference(self, referenceDict):
+        self.__referenceDict = referenceDict
